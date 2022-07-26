ac8d9f4aaef582507ed204c0f2be63f3ae6b3ca5
swagata <swagata@DESKTOP-31OSC0L>
2022-07-26 20:29:05 +0530

    this is my second commit

---
+++ src/monday/Sample2.java
@@ -0,0 +1,12 @@
+package monday;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java ");
+		
+		System.out.println("welcome to github ");
+
+	}
+
+}
